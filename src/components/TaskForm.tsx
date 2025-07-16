f85b52b5da16cc9d7240afb8def96fafff3f43b3
Vandana <vandanadev96@gmail.com>
2025-07-16 09:49:54 +0530

    fix: remove comment

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,6 +1,6 @@
-import dayjs from 'dayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from "dayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import React, { useState } from "react";
 import { useForm, Controller } from "react-hook-form";
 import { useDispatch } from "react-redux";
@@ -8,7 +8,7 @@ import { addTaskAsync, editTaskAsync, Task } from "../store/tasksSlice";
 import { Box, Typography } from "@mui/material";
 import { v4 as uuidv4 } from "uuid";
 import TextField from "../ui/components/TextField";
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import Button from "../ui/components/Button";
 import Select from "../ui/components/Select";
 import CircularProgress from "../ui/components/CircularProgress";
@@ -55,103 +55,65 @@ const TaskForm: React.FC<Props> = ({ editTask, onClose }) => {
       dueDate: "",
     },
   });
-const onSubmit = async (data: TaskFormInputs) => {
-  setLoading(true);
-  try {
-    if (editTask) {
-      await new Promise((resolve) => setTimeout(resolve, 700));
-      await dispatch(editTaskAsync({ ...editTask, ...data }));
-      setLoading(false);
+  const onSubmit = async (data: TaskFormInputs) => {
+    setLoading(true);
+    try {
+      if (editTask) {
+        await new Promise((resolve) => setTimeout(resolve, 700));
+        await dispatch(editTaskAsync({ ...editTask, ...data }));
+        setLoading(false);
+        setSnackbar({
+          open: true,
+          message: "Task updated",
+          severity: "success",
+        });
+        setTimeout(() => {
+          if (onClose) onClose(); // Close dialog after snackbar is visible
+        }, 700);
+        setTimeout(() => {
+          setSnackbar({ open: false, message: "", severity: "info" });
+        }, 2000);
+      } else {
+        const newTask: Task = {
+          id: uuidv4(),
+          ...data,
+        };
+        await new Promise((resolve) => setTimeout(resolve, 700));
+        await dispatch(addTaskAsync(newTask));
+        setSnackbar({ open: true, message: "Task added", severity: "success" });
+        setLoading(false);
+        setTimeout(
+          () => setSnackbar({ open: false, message: "", severity: "info" }),
+          2000
+        );
+        reset();
+      }
+    } catch {
       setSnackbar({
         open: true,
-        message: "Task updated",
-        severity: "success",
+        message: "Error saving task",
+        severity: "error",
       });
-      setTimeout(() => {
-        if (onClose) onClose(); // Close dialog after snackbar is visible
-      }, 700);
-      setTimeout(() => {
-        setSnackbar({ open: false, message: "", severity: "info" });
-      }, 2000);
-    } else {
-      const newTask: Task = {
-        id: uuidv4(),
-        ...data,
-      };
-      await new Promise((resolve) => setTimeout(resolve, 700));
-      await dispatch(addTaskAsync(newTask));
-      setSnackbar({ open: true, message: "Task added", severity: "success" });
       setLoading(false);
       setTimeout(
         () => setSnackbar({ open: false, message: "", severity: "info" }),
         2000
       );
-      reset();
     }
-  } catch {
-    setSnackbar({
-      open: true,
-      message: "Error saving task",
-      severity: "error",
-    });
-    setLoading(false);
-    setTimeout(
-      () => setSnackbar({ open: false, message: "", severity: "info" }),
-      2000
-    );
-  }
-};
-
-  // const onSubmit = async (data: TaskFormInputs) => {
-  //   setLoading(true);
-  //   try {
-  //     if (editTask) {
-  //       await new Promise((resolve) => setTimeout(resolve, 700));
-  //       await dispatch(editTaskAsync({ ...editTask, ...data }));
-  //       setSnackbar({
-  //         open: true,
-  //         message: "Task updated",
-  //         severity: "success",
-  //       });
-  //       setLoading(false);
-  //       if (onClose) onClose();
-  //       setTimeout(
-  //         () => setSnackbar({ open: false, message: "", severity: "info" }),
-  //         2000
-  //       );
-  //     } else {
-  //       const newTask: Task = {
-  //         id: uuidv4(),
-  //         ...data,
-  //       };
-  //       await new Promise((resolve) => setTimeout(resolve, 700));
-  //       await dispatch(addTaskAsync(newTask));
-  //       setSnackbar({ open: true, message: "Task added", severity: "success" });
-  //       setLoading(false);
-  //       setTimeout(
-  //         () => setSnackbar({ open: false, message: "", severity: "info" }),
-  //         2000
-  //       );
-  //       reset();
-  //     }
-  //   } catch {
-  //     setSnackbar({
-  //       open: true,
-  //       message: "Error saving task",
-  //       severity: "error",
-  //     });
-  //     setLoading(false);
-  //     setTimeout(
-  //       () => setSnackbar({ open: false, message: "", severity: "info" }),
-  //       2000
-  //     );
-  //   }
-  // };
+  };
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <Box sx={{ boxShadow: editTask ? 0 : 2 , p:editTask ? 0 : 4, borderRadius: editTask ? 0 : 1, mb: 4, bgcolor: '#fff' }}>
-        <Typography variant="h4" textAlign={'center'} gutterBottom>
+      <Box
+        sx={{
+          boxShadow: editTask ? 0 : 2,
+          p: editTask ? 0 : 4,
+          borderRadius: editTask ? 0 : 1,
+          mb: 4,
+          bgcolor: "#fff",
+        }}
+      >
+        <Typography variant="h4" textAlign={"center"} gutterBottom>
           {editTask ? "Edit Task" : "Add New Task"}
         </Typography>
         <form onSubmit={handleSubmit(onSubmit)}>
@@ -162,7 +124,11 @@ const onSubmit = async (data: TaskFormInputs) => {
             render={({ field }) => (
               <TextField
                 {...field}
-                sx={{ background: 'transparent', borderRadius: 0, boxShadow: 'none' }}
+                sx={{
+                  background: "transparent",
+                  borderRadius: 0,
+                  boxShadow: "none",
+                }}
                 label="Task Name"
                 error={!!errors.name}
                 helperText={errors.name?.message}
@@ -177,7 +143,11 @@ const onSubmit = async (data: TaskFormInputs) => {
             render={({ field }) => (
               <TextField
                 {...field}
-                sx={{ background: 'transparent', borderRadius: 0, boxShadow: 'none' }}
+                sx={{
+                  background: "transparent",
+                  borderRadius: 0,
+                  boxShadow: "none",
+                }}
                 label="Description"
                 color="info"
                 error={!!errors.description}
@@ -196,7 +166,7 @@ const onSubmit = async (data: TaskFormInputs) => {
                 options={priorities}
                 error={!!errors.priority}
                 color="info"
-                sx={{ background: 'transparent' }}
+                sx={{ background: "transparent" }}
               />
             )}
           />
@@ -209,8 +179,10 @@ const onSubmit = async (data: TaskFormInputs) => {
                 label="Due Date"
                 format="YYYY-MM-DD"
                 value={field.value ? dayjs(field.value) : null}
-                onChange={(date: dayjs.Dayjs | null) => field.onChange(date ? date.format('YYYY-MM-DD') : '')}
-                minDate={dayjs().startOf('day')}
+                onChange={(date: dayjs.Dayjs | null) =>
+                  field.onChange(date ? date.format("YYYY-MM-DD") : "")
+                }
+                minDate={dayjs().startOf("day")}
                 open={datePickerOpen}
                 onOpen={() => setDatePickerOpen(true)}
                 onClose={() => setDatePickerOpen(false)}
@@ -218,8 +190,14 @@ const onSubmit = async (data: TaskFormInputs) => {
                   textField: {
                     error: !!errors.dueDate,
                     helperText: errors.dueDate?.message,
-                    color: 'info',
-                    sx: { background: 'transparent', borderRadius: 0, boxShadow: 'none', width: '100%', mt: 2 },
+                    color: "info",
+                    sx: {
+                      background: "transparent",
+                      borderRadius: 0,
+                      boxShadow: "none",
+                      width: "100%",
+                      mt: 2,
+                    },
                     InputLabelProps: { shrink: true },
                     onClick: () => setDatePickerOpen(true),
                   },
@@ -227,7 +205,13 @@ const onSubmit = async (data: TaskFormInputs) => {
               />
             )}
           />
-          <Button type="submit" color="secondary"  variant="contained" disabled={loading} sx={{ mt: 2 } }>
+          <Button
+            type="submit"
+            color="secondary"
+            variant="contained"
+            disabled={loading}
+            sx={{ mt: 2 }}
+          >
             {loading ? (
               <CircularProgress />
             ) : editTask ? (
